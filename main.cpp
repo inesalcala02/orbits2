39ced24c4dd94b5ae612a6fe391f532c1b7a97bf
inesalcala02 <821136@unizar.es>
2023-07-12 21:16:26 +0200

    orbits 2 modificación en los algoritmos de euler simpléctico y midpoint

--- main.cpp
+++ main.cpp
@@ -89,7 +89,7 @@ ImpMidpoint(steps[1], h[1]);
 puts("Método implicit midpoint acabado ... ");
 getchar();
 
-///SYMPLECTIC EULER
+///SYMPLECTIC EULER (sale)
 //SympcEuler(steps[1],h[1]);
 puts("Método symplectic Euler acabado ... ");
 getchar();
@@ -127,7 +127,7 @@ energ[0]=Energia(x[0],y[0],vx[0],vy[0]);
 x[1]= x[0] + h*vx[0];
 y[1]= y[0] + h*vy[0];
 
-
+printf("ha entrado");
 
 
 for(int i=1; i<pasos-1; i++){
@@ -222,6 +222,7 @@ float k2_vy[pasos];
 float energ[pasos];
 float err[pasos];
 float t=0;
+float aux;
 
 for (int i=1; i<pasos; i++){  //inicializar vectores
    vx[i]=0;
@@ -244,15 +245,46 @@ for (int i=1; i<pasos; i++){  //inicializar vectores
 }
 
 x[0]= 1-e;
-y[0]=0;
+y[0]=0.01;
 vx[0]=0;
 vy[0]=sqrt((1.0 +e)/(1.0 -e));
 energ[0]=-0.5;
+aux=0;
+for(int i=0; i<pasos-1;i++){
 
-for(int i=1; i<pasos;i++){
-t=i*h;
+k1_vx[i]=f_Kepler(x[i],y[i]);
+k1_vy[i]=f_Kepler(y[i],x[i]);
+
+k1_x[i]= x[i] + h*(vx[i] + 0.5*h*k1_vx[i]);
+k1_y[i]= y[i] + h*(vy[i] + 0.5*h*k1_vy[i]);
+
+k2_vx[i]=f_Kepler(k1_x[i],k1_y[i]);
+k2_vy[i]=f_Kepler(k1_y[i],k1_x[i]);
+
+x[i+1]=x[i] + h*vx[i] + 0.5*h*h*k1_vx[i];
+y[i+1]=y[i] + h*vy[i] + 0.5*h*h*k2_vy[i];
+
+vx[i+1]=vx[i]+ 0.5*h*(k1_vx[i]+k2_vx[i]);
+vy[i+1]=vy[i]+ 0.5*h*(k1_vy[i]+k2_vy[i]);
+/*
+
+
+k2_vx[i-1]=h*f_Kepler(x[i-1]+0.5*h*vx[i-1], y[i-1]+0.5*h*vy[i-1]);
+k2_vy[i-1]=h*f_Kepler(y[i-1]+0.5*h*vy[i-1], x[i-1]+0.5*h*vx[i-1]);
+
+vx[i]=vx[i-1]+k2_vx[i-1];
+vy[i]=vy[i-1]+k2_vy[i-1];
+
+
+k2_x[i-1]=h*(velocidad(h*0.5,x[i-1]+0.5*h*vx[i-1],y[i-1]+0.5*h*vy[i-1]) + vx[i-1]);
+k2_y[i-1]=h*(velocidad(h*0.5,y[i-1]+0.5*h*vy[i-1],x[i-1]+0.5*h*vx[i-1]) + vy[i-1]);
+
+x[i]=x[i-1]+k2_x[i-1];
+y[i]=y[i-1]+k2_y[i-1];
+
+t=i*h;*/
 //printf("tiempo %f",t);
-
+/*
 kx[i-1]= x[i-1]+h*0.5*vx[i-1];
 ky[i-1]= y[i-1]+h*0.5*vy[i-1];
 
@@ -264,7 +296,9 @@ k1_vy[i-1]=vy[i-1]+0.5*h*f_Kepler(y[i-1],x[i-1]);
 
 
 x[i]=x[i-1] + h*0.5*(vx[i-1]+vx[i]);
-y[i]=y[i-1] + h*0.5*(vy[i-1]+vy[i]);
+y[i]=y[i-1] + h*0.5*(vy[i-1]+vy[i]);*/
+
+
 
 
 energ[i]=Energia(x[i],y[i],vx[i],vy[i]);
@@ -285,8 +319,6 @@ float vx[pasos];
 float vy[pasos];
 float energ[pasos];
 float err[pasos];
-float x_aux[pasos];
-float y_aux[pasos];
 
 for (int i=0; i<pasos; i++){  //inicializar vectores
    x[i]=0;
@@ -295,8 +327,6 @@ for (int i=0; i<pasos; i++){  //inicializar vectores
    vy[i]=0;
    energ[i]=0;
    err[i]=0;
-   x_aux[i]=0;
-   y_aux[i]=0;
 }
 
 x[0]= 1-e;
@@ -306,22 +336,21 @@ vy[0]=sqrt((1.0 +e)/(1.0 -e));
 
 energ[0]=Energia(x[0],y[0],vx[0],vy[0]);
 
-for(int i=0; i<pasos-1; i++){
+for(int i=0; i<pasos-1; i++){  //x explícita e y implícita
 
-x_aux[i]= x[i] + h*vx[i];
-y_aux[i]=y[i]+h*vy[i];
+x[i+1]=x[i]+h*vx[i]; //exp
+y[i+1]=y[i]+h*vy[i]; //imp
 
-vx[i+1]=vx[i]+h*f_Kepler(x[i],y_aux[i]);
-vy[i+1]=vy[i]+h*f_Kepler(y[i],x_aux[i]);
+vx[i+1]=vx[i]+h*f_Kepler(x[i+1],y[i+1]);
+vy[i+1]=vy[i]+h*f_Kepler(y[i+1],x[i+1]);
 
 
-x[i+1]=x[i]+h*vx[i+1];
-y[i+1]=y[i]+h*vy[i+1];
+
 
 energ[i]=Energia(x[i],y[i],vx[i],vy[i]);
 err[i]=Calcerror(x[i],y[i]);
 
-printf("Paso %d. X=%f  Y= %f Energia=%f  INI X0 = %f Y0 =%f\n", i, x[i], y[i], energ[i], x[0],y[0]);
+//printf("Paso %d. X=%f  Y= %f Energia=%f  INI X0 = %f Y0 =%f\n", i, x[i], y[i], energ[i], x[0],y[0]);
 
 }
 printf("Valores iniciales X = %f Y = %f", x[0],y[0]);
@@ -332,11 +361,11 @@ printEsymp(x,y,energ,err,pasos);
 void printEexplicito(float x[], float y[],float ener[],float err[], int pasos){
     float aux;
 FILE *f1;
- f1=fopen("Euler_explicito.txt", "w");
+ f1=fopen("Euler_explicito00001.txt", "w");
 	if(f1==NULL){printf("Error al abrir el archivo de texto.");exit(1);	}
 
     for(int i=0;i<pasos;i++){
-            aux=i*0.0005;
+            aux=i*0.0001;
 
             fprintf(f1,"%f   %f    %f   %f   %f\n",aux,x[i],y[i],ener[i], err[i]);
     }
@@ -353,7 +382,7 @@ FILE *f2;
 	if(f2==NULL){printf("Error al abrir el archivo de texto.");exit(1);	}
 
     for(int i=0;i<pasos;i++){
-            aux=i*0.05;
+            aux=i*0.001;
             fprintf(f2,"%f   %f    %f   %f   %f\n",aux,x[i],y[i],ener[i], err[i]);
 
     }
@@ -366,7 +395,7 @@ FILE *f2;
 void printMidpoint(float x[], float y[],float ener[],float err[], int pasos){
     float aux;
 FILE *f3;
- f3=fopen("Midpoint.txt", "w");
+ f3=fopen("Midpoint1.txt", "w");
 	if(f3==NULL){printf("Error al abrir el archivo de texto.");exit(1);	}
 
     for(int i=0;i<pasos;i++){
